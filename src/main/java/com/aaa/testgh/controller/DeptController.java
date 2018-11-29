5fe45a248d1e18d81d7210ed077a5f805428fd57
a291434755 <809605017@qq.com>
2018-11-29 10:48:16 +0800

    dev分支测试

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -10,7 +10,7 @@ package com.aaa.testgh.controller;
 public class DeptController {
 
     public static void main(String[] args) {
-
+        System.out.println("新加分支");
         System.out.println("测试github提交");
     }
 }
